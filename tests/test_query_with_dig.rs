d721718516a46ecdc4ed42d223494b439b4b69e9
myl7 <myl.ustc@gmail.com>
2020-12-10 02:43:26 +0800

    Add dig integrated test.

---
+++ tests/test_query_with_dig.rs
@@ -0,0 +1,24 @@
+use std::process::{Command, Stdio};
+
+#[test]
+fn main() {
+    let mut relay_handle = Command::new("target/debug/dnsl")
+        .env("DNSL_LISTEN", "127.0.0.1:10000")
+        .env("DNSL_LOOKUP", "0.0.0.0:10001")
+        .env("DNSL_UPSTREAM", "8.8.8.8:53")
+        .spawn()
+        .unwrap();
+    let query1 = Command::new("dig")
+        .args(&["myl.moe", "@127.0.0.1", "-p", "10000"])
+        .stdout(Stdio::piped())
+        .status()
+        .unwrap();
+    assert!(query1.success());
+    let query2 = Command::new("dig")
+        .args(&["www.google.com", "@127.0.0.1", "-p", "10000"])
+        .stdout(Stdio::piped())
+        .status()
+        .unwrap();
+    assert!(query2.success());
+    relay_handle.kill().unwrap();
+}
